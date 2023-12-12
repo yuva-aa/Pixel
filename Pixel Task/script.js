af8df489d14fd3d6ea6c05deb055036cac836bdb
Yuva_aa <152509708+yuva-aa@users.noreply.github.com>
2023-12-12 16:18:19 +0530

    Add files via upload

---
+++ Pixel Task/script.js
@@ -0,0 +1,17 @@
+var SectionSlider = new Swiper('.section-3-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
